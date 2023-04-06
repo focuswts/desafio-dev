ab6e119949e8fefab5fedf60f0e60c0d93ffa5a4
Felipe Bagatim <felipebg2012@gmail.com>
2023-04-05 23:29:49 -0300

    Adicionada consulta para filtrar as transacoes por nome de loja

--- src/main/java/com/bgt/desafio/repository/CnabTransactionRepository.java
+++ src/main/java/com/bgt/desafio/repository/CnabTransactionRepository.java
@@ -4,6 +4,11 @@ import com.bgt.desafio.entity.CnabTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CnabTransactionRepository extends JpaRepository<CnabTransactionEntity, Long> {
+
+    List<CnabTransactionEntity> findAllByStoreName(String storeName);
+
 }
